ce31fd6fa58b22a0dad65a039774f69bb800aa53
SageM <madduxsage@gmail.com>
2023-11-18 20:52:59 -0500

    routes complete, serializers complete. All data displayed in event url. Need CORS

--- tik_it/serializers.py
+++ tik_it/serializers.py
@@ -4,25 +4,6 @@ from .models import Venue, Event
 
 
 
-class EventSerializer(serializers.ModelSerializer):
-
-    venue = serializers.HyperlinkedRelatedField(
-        view_name='venue_detail',
-        read_only=True
-    )
-
-    venue_id = serializers.PrimaryKeyRelatedField(
-        queryset=Venue.objects.all(),
-        source='venue'
-    )
-
-
-    class Meta:
-        model = Event
-        fields = ('id', 'event_name', 'date','venue', 'venue_id')
-
-
-
 class VenueSerializer(serializers.ModelSerializer):
   
     venue_url = serializers.ModelSerializer.serializer_url_field(
@@ -33,4 +14,30 @@ class VenueSerializer(serializers.ModelSerializer):
     class Meta:
        model = Venue
        fields = ('id', 'name', 'location', 'photo_url', 'description','price','max_occupancy','rating','number_of_rating', 'venue_url')
+
+
+
+
+class EventSerializer(serializers.ModelSerializer):
+# when using nested serializer the name you are declaring it as has to be the actual attribute name in the model. So for instance venue here is refering to the venue FK in models.py. This is tricky.
+    venue = VenueSerializer(
+
+    )
+# Here this was a hyper link when venue1 was passed into fields, however now that the above serializer has been put in place it no longer works. This would just create a link in the api to the venu details page of the venue ID that was selected.
+    # venue1 = serializers.HyperlinkedRelatedField(
+    #     view_name='venue_detail',
+    #     read_only=True
+    # )
+
+
+#This gives you the venue ID in the event Ser.
+    venue_id = serializers.PrimaryKeyRelatedField(
+        queryset=Venue.objects.all(),
+        source='venue'
+    )
+
+
+    class Meta:
+        model = Event
+        fields = ('id', 'event_name', 'date', 'venue_id', 'venue')
    
